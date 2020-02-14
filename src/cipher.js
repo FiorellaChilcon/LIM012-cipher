7e86697ce16578e9adc1e0b8bcd7ce7072d94757
fiorella <fiorellachilc_20@hotmail.com>
2020-02-14 12:27:34 -0500

    - tests, corregir errores,  - mejorar el UI

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
+  encode: (number, str) => {
+    const offset = parseInt(number);
+    const text = str.toUpperCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text.charAt(i) == " ") {
+        result += " ";
+      } else {
+        let letterCode = text.charCodeAt(i);
+        let position = (letterCode - 65 + offset) % 26 + 65;
+        let finalLetter = String.fromCharCode(position);
+        result += finalLetter;
+      }
+    }
+    return result;
+  },
+  decode: (number, str) => {
+    const offset = parseInt(number);
+    const text = str.toUpperCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text.charAt(i) == " ") {
+        result += " ";
+      } else {
+        let letterCode = text.charCodeAt(i);
+        let position = (letterCode - 65 - offset) % 26 + 65;
+        let finalLetter = String.fromCharCode(position);
+        result += finalLetter;
+      }
+    }
+    return result;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let count = 0;
+const changeIt = () => {
+   count++;
+   const show1 = document.getElementById("show1");
+   const show2 = document.getElementById("show2");
+   const change = document.getElementById("change");
+   if (count % 2 !== 0) {
+      show1.style.display = "none";
+      show2.style.display = "flex";
+      change.style.backgroundColor = "#45FF16";
+   } else {
+      show1.style.display = "flex";
+      show2.style.display = "none";
+      change.style.backgroundColor = "yellow";
+   }
+}
+
+const change = document.getElementById("change");
+change.addEventListener("click", changeIt);
+
+
+
+const show1 = document.getElementById("show1");
+const second= document.getElementById("second");
+
+show1.addEventListener("click", () => {
+   const numero = document.getElementById("numberSelected").value;
+   const cadena = document.getElementById("first").value;
+
+   second.value=cipher.encode(numero, cadena);
+});
+
+const show2 = document.getElementById("show2");
+show2.addEventListener("click", () => {
+   const numero = document.getElementById("numberSelected").value;
+   const cadena = document.getElementById("first").value;
+   second.value = cipher.decode(numero, cadena);
+});
