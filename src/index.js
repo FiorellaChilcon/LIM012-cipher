f48b33a201852c15e88198d93ec44a599c4370ca
fiorella <fiorellachilc_20@hotmail.com>
2020-02-28 11:06:37 -0500

    final

--- src/index.js
+++ src/index.js
@@ -21,12 +21,6 @@ const changeIt = () => {
     option2.innerHTML = 'CIFRADO';
   }
 };
-// let value = true;
-// const changeIt = () => {
-//    value = value? false : true ;
-//    console.log(value);
-//    return value;
-// }
 change.addEventListener('click', changeIt);
 const show1 = document.getElementById('show1');
 const second = document.getElementById('second');
@@ -35,21 +29,32 @@ show1.addEventListener('click', () => {
   const cadena = document.getElementById('first').value;
   second.value = cipher.encode(numero, cadena);
 });
-// let valor = changeIt();
-// show1.addEventListener("click", () => {
-//    if(valor==false) {
-//       const numero = document.getElementById("numberSelected").value;
-//       const cadena = document.getElementById("first").value;
-//       second.value = cipher.encode(numero, cadena);
-//    } else if(valor==true) {
-//       const numero = document.getElementById("numberSelected").value;
-//       const cadena = document.getElementById("first").value;
-//       second.value = cipher.decode(numero, cadena);
-//    }
-// });
 const show2 = document.getElementById('show2');
 show2.addEventListener('click', () => {
   const numero = document.getElementById('numberSelected').value;
   const cadena = document.getElementById('first').value;
   second.value = cipher.decode(numero, cadena);
 });
+// const change = document.getElementById('change');
+// let value = true;
+// const changeIt = () => {
+//   value = value ? false : true;
+//   console.log(value);
+//   return value;
+// }
+// change.addEventListener('click', changeIt);
+// const valor = changeIt();
+// const show1 = document.getElementById('show1');
+// const second = document.getElementById('second');
+// show1.addEventListener("click", () => {
+//   if (valor === false) {
+//     const numero = document.getElementById("numberSelected").value;
+//     const cadena = document.getElementById("first").value;
+//     second.value = cipher.encode(numero, cadena);
+//   } 
+//   if (valor === true) {
+//     const numero = document.getElementById("numberSelected").value;
+//     const cadena = document.getElementById("first").value;
+//     second.value = cipher.decode(numero, cadena);
+//   }
+// });
